358840b4ba353dfeb03c42f4e790a13d36797145
cdxvy30 <cdxvy30@gmail.com>
2023-01-10 21:09:51 +0800

    更改檔名

---
+++ middleware/validation.js
@@ -0,0 +1,33 @@
+module.exports = (req, res, next) => {
+
+  const { name, corporation, email, password } = req.body;
+
+  function validEmail(userEmail) {  // check if the email is in correct format
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail); // 正規表達式
+  }
+
+  if (req.path === "/register") {
+    if (![name, corporation, email, password].every(Boolean)) {
+      
+      return res.status(401).json("註冊:缺少憑證");
+
+    } else if (!validEmail(email)) {
+      
+      return res.status(401).json("註冊:信箱格式不合法");
+
+    }
+  } else if (req.path === "/login") {
+    if (![email, password].every(Boolean)) {
+      
+      return res.status(401).json("登入:缺少憑證");
+
+    } else if (!validEmail(email)) {
+      
+      return res.status(401).json("登入:信箱格式錯誤");
+
+    }
+  }
+
+  // 呼叫中介軟體的下個函式
+  next(); 
+};
