abb7d7005aa5cfac9bb2dd80ed1d5ef7c11e38a9
cdxvy30 <cdxvy30@gmail.com>
2023-03-14 02:36:44 +0800

    續前

---
+++ routes/manufacturers.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const pool = require('../services/pool');
+
+router.post('/', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+});
+
+
+router.get('/', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+});
+
+module.exports = router;
