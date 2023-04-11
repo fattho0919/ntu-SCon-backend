780f3d3a5a51101600c13da27faed4f5dc818f31
cdxvy30 <cdxvy30@gmail.com>
2023-04-11 10:24:14 +0800

    Install excel4node & create a route for exporting excel

--- routes/sheet.js
+++ routes/sheet.js
@@ -1,17 +1,27 @@
 const router = require('express').Router();
 const pool = require('../services/pool');
 const authorization = require('../middleware/authorization');
+const xl = require('excel4node');
+const wb = new xl.Workbook();
 
 router.get('/:projectId', async (req, res) => {
   try {
     const projectId = req.params.projectId;
     
+    // Select all issues
     const data = await pool.query(
       `SELECT * FROM issues WHERE project_id = $1`,
       [projectId]
     );
 
-    
+    // **************************************** //
+    // Write code here 
+
+
+    // **************************************** //
+
+    // Response excel file
+    wb.write('ExcelFile.xlsx', res);
 
   } catch (error) {
     res.status(500).json('伺服器錯誤');
